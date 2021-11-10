9e955ba3ce74fc740fb8d843a4fc8d6622e1e9f7
Hugo Leonardo <hugoleonardo.dev@gmail.com>
2021-11-10 17:26:01 -0300

    :construction: update stylelint to v14

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,16 @@
-import styled, { ThemeProvider } from 'styled-components'
 import React from 'react'
-import { Provider, useSelector } from 'react-redux'
+import { Provider } from 'react-redux'
 
 import store from './store'
 
 import Routes from './Routes'
 
-import { RootStateWithReducers } from 'store/constants/_rootReducerTypes'
-
-import darkTheme from 'common/themes/darkTheme'
-// import defaultTheme from 'common/themes/defaultTheme'
-import goodContrastTheme from 'common/themes/goodContrastTheme'
-
-const RootWrapper = styled.div`
-    background: ${properties => properties.theme.colors.background.light};
-    color: ${properties => properties.theme.colors.text.light};
-`
 import './App.scss'
 
 const App: React.FC = () => {
-    const { switchTheme } = useSelector((state: RootStateWithReducers) => state.teachers)
     return (
         <Provider store={store}>
-            <ThemeProvider theme={switchTheme ? darkTheme : goodContrastTheme}>
-                <RootWrapper>
-                    <Routes />
-                </RootWrapper>
-            </ThemeProvider>
+            <Routes />
         </Provider>
     )
 }
--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,26 +1,46 @@
+import styled, { ThemeProvider } from 'styled-components'
 import { ConnectedRouter } from 'connected-react-router'
 import { Route, Switch } from 'react-router-dom'
 import React from 'react'
+import { useSelector } from 'react-redux'
+
+import darkTheme from 'common/themes/darkTheme'
+import goodContrastTheme from 'common/themes/goodContrastTheme'
 
 const HomePage = React.lazy(() => import('pages/HomePage'))
 const TeacherPage = React.lazy(() => import('pages/TeacherPage'))
 
 import { history } from './store'
 
+import { RootStateWithReducers } from 'store/constants/_rootReducerTypes'
+
+const RootWrapper = styled.div`
+    background: ${properties => properties.theme.colors.background.light};
+    color: ${properties => properties.theme.colors.text.light};
+`
 const Routes: React.FC = () => {
+    const { switchTheme } = useSelector((state: RootStateWithReducers) => state.teachers)
     return (
-        <React.Suspense fallback={<div>Carregando...</div>}>
+        <ThemeProvider theme={switchTheme ? darkTheme : goodContrastTheme}>
             <ConnectedRouter history={history}>
-                <Switch>
-                    <Route exact path="/">
-                        <HomePage />
-                    </Route>
-                    <Route path="/teacher-app">
-                        <TeacherPage />
-                    </Route>
-                </Switch>
+                <RootWrapper>
+                    <React.Suspense fallback={<div>Carregando...</div>}>
+                        <Switch>
+                            <Route exact path="/">
+                                <React.Suspense fallback={<div>Carregando...</div>}>
+                                    <HomePage />
+                                </React.Suspense>
+                            </Route>
+                            <Route path="/teacher-app">
+                                <React.Suspense fallback={<div>Carregando...</div>}>
+                                    <TeacherPage />
+                                </React.Suspense>
+                            </Route>
+                        </Switch>
+                    </React.Suspense>
+                </RootWrapper>
             </ConnectedRouter>
-        </React.Suspense>
+        </ThemeProvider>
     )
 }
 
